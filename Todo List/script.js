495d0c2d0f9a9162fb0c660a01f9d3a53fa84124
Jean Ponomarev <Jean.Ponomarev@nordigy.ru>
2020-08-25 00:08:45 +0300

    Исправленная версия задачи Todo list

--- Todo List/script.js
+++ Todo List/script.js
@@ -5,17 +5,17 @@ document.addEventListener("DOMContentLoaded", function () {
         todoItem.appendChild(currentDeleteBlock);
     }
 
-    var addTodoButton = document.getElementById("add_todo_button");
-    var todoInputText = document.getElementById("todo_input_text");
+    var addTodoButton = document.querySelector(".add_todo_button");
+    var todoInputText = document.querySelector(".todo_input_text");
     var todoList = document.getElementById("todo_list");
 
     var listItemHtml = "<div class='list_item_text'></div>" +
-        "<button type='button' class='edit_button'><i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i></button>" +
-        "<button type='button' class='delete_button'><i class=\"fa fa-trash\"></i></button>";
+        "<button type='button' class='edit_button' title='edit note'><i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i></button>" +
+        "<button type='button' class='delete_button' title='delete note'><i class=\"fa fa-trash\"></i></button>";
 
     var editListItemHtml = "<input type='text' class='list_item_input_text'>" +
-        "<button type='button' class='save_button'><i class=\"fa fa-floppy-o\" aria-hidden=\"true\"></i></button>" +
-        "<button type='button' class='cancel_button'><i class=\"fa fa-arrow-up\" aria-hidden=\"true\"></i></button>";
+        "<button type='button' class='save_button' title='save changes'><i class=\"fa fa-floppy-o\" aria-hidden=\"true\"></i></button>" +
+        "<button type='button' class='cancel_button' title='cancel changes'><i class=\"fa fa-arrow-up\" aria-hidden=\"true\"></i></button>";
 
     var initialText;
 
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
         var inputText = todoInputText.value;
 
         if (!inputText.replace(/\s/g, '').length) {
-            alert("Input filed is empty");
+            alert("Input field is empty");
             return;
         }
 
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         currentDeleteBlock.addEventListener("click", function () {
             todoItem.remove();
-        })
+        });
 
         var currentEditBlock = todoItem.querySelector(".edit_button, .fa-pencil-square-o");
 
@@ -48,20 +48,26 @@ document.addEventListener("DOMContentLoaded", function () {
             initialText = todoItem.querySelector(".list_item_text").innerHTML;
             todoItem.innerHTML = editListItemHtml;
             todoItem.querySelector(".list_item_input_text").value = initialText;
-            todoItem.querySelector(".list_item_input_text").style.color = "#228B22";
+            todoItem.querySelector(".list_item_input_text").classList.add("changed_text");
 
             var currentSaveBlock = todoItem.querySelector(".save_button, .fa-floppy-o");
             currentSaveBlock.addEventListener("click", function () {
                 var currentText = todoItem.querySelector(".list_item_input_text").value;
-                insertHtmlAfterEditOperation(todoItem, currentEditBlock, currentDeleteBlock)
-                todoItem.querySelector(".list_item_text").innerHTML = currentText;
-            })
 
-            var currentResetBlock = todoItem.querySelector(".cancel_button, .fa-arrow-up")
+                if (!currentText.replace(/\s/g, '').length) {
+                    alert("Input field is empty");
+                    return;
+                }
+
+                insertHtmlAfterEditOperation(todoItem, currentEditBlock, currentDeleteBlock);
+                todoItem.querySelector(".list_item_text").innerHTML = currentText;
+            });
+
+            var currentResetBlock = todoItem.querySelector(".cancel_button, .fa-arrow-up");
             currentResetBlock.addEventListener("click", function () {
-                insertHtmlAfterEditOperation(todoItem, currentEditBlock, currentDeleteBlock)
+                insertHtmlAfterEditOperation(todoItem, currentEditBlock, currentDeleteBlock);
                 todoItem.querySelector(".list_item_text").innerHTML = initialText;
-            })
+            });
         });
     });
 });
